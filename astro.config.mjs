31c02416c0159af5e70b555eb09ed413bcaee6c8
Gr0bGr0b <rob.jacq@protonmail.com>
2026-05-23 21:31:25 +0200

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://gr0bgr0b.github.io',
+  site: 'https://gr0bgr0b.github.io',
   base: '/is-it-hot'
 });
